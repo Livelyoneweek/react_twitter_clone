65ecb3020e1b1099299a3e9224dfab8c2d8d8c0e
dorosee-bjchoi <bj_choi@dorosee.kr>
2023-12-28 14:54:47 +0900

    feat: github login add

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -4,6 +4,7 @@ import {createUserWithEmailAndPassword, updateProfile} from "firebase/auth"
 import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
 import { Error, Form, Input, Switcher, Title, Wrapper } from "../components/auth-components";
+import GithubButton from "../components/github-btn";
 
 
 export default function CreateAccount() {
@@ -88,6 +89,7 @@ export default function CreateAccount() {
       <Switcher>
         Already have an account? <Link to="/login">Log in &rarr;</Link>
       </Switcher>
+      <GithubButton />
     </Wrapper>
   );
 }
