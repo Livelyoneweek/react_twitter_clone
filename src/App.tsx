c40d62d7a1be32664d51012cf83aceceba87c138
Livelyoneweek <wlvvwhs@naver.com>
2024-01-06 13:58:53 +0900

    feat: db query add

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,11 @@ const GlobalStyles = createGlobalStyle`
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, 
     Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
+  /* ::-webkit-scrollbar {
+    display:none;
+  }  */
+
+  
 `;
 
 const Wrapper = styled.div`
