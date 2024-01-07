dcc01af0c531a315f815bb3642eb5a6bb479ea18
Livelyoneweek <wlvvwhs@naver.com>
2024-01-08 01:04:24 +0900

    feat: real time timeline add

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,8 +1,16 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
 import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
 
 export type ITweet = {
   id: string;
@@ -13,33 +21,49 @@ export type ITweet = {
   createdAt: number;
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+`;
 
 export default function Timeline() {
   const [tweets, setTweet] = useState<ITweet[]>([]);
 
   const fetchTweets = async () => {
+    let unsubscribe: Unsubscribe | null = null;
     const tweetsQuery = query(
       collection(db, "tweets"),
-      orderBy("createdAt", "desc")
+      orderBy("createdAt", "desc"),
+      limit(25)
     );
 
-    const snapshot = await getDocs(tweetsQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      const { tweet, createdAt, userId, username, photo } = doc.data();
-      return {
-        tweet,
-        createdAt,
-        userId,
-        username,
-        photo,
-        id: doc.id,
-      };
+    unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+      const tweets = snapshot.docs.map((doc) => {
+        const { tweet, createdAt, userId, username, photo } = doc.data();
+        return {
+          tweet,
+          createdAt,
+          userId,
+          username,
+          photo,
+          id: doc.id,
+        };
+      });
+      setTweet(tweets);
     });
-    setTweet(tweets);
+    return unsubscribe;
   };
   useEffect(() => {
-    fetchTweets();
+    let unsubscribe: Unsubscribe | null = null;
+
+    (async () => {
+      unsubscribe = await fetchTweets();
+    })();
+
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>
